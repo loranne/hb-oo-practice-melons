7dfcd8713374b2122c08601a4d58f7ce385a0198
Loranne Nasir <loranne.n@gmail.com>
2020-10-08 20:01:59 +0000

    made changes through line 33

--- harvest.py
+++ harvest.py
@@ -6,25 +6,31 @@
 class MelonType(object):
     """A species of melon at a melon farm."""
 
-    def __init__(self, code, first_harvest, color, is_seedless=False, is_bestseller=False, 
-                 name):
+    def __init__(self, code, first_harvest, color, name, is_seedless=False, is_bestseller=False, 
+                ):
         """Initialize a melon."""
 
-        self.pairings = []
-
+        self.pairings = ['ice cream','proscuitto', 'mint','strawberries and mint']
         self.code = code
         self.first_harvest = first_harvest
         self.color = color
         self.name = name
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
         
         # Fill in the rest
-
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
         # use this function to populate list of self.pairings
-        self.pairs = pairing
-        self.pairing.append(pairing)
-
+        # self.pairs = pairing
+        self.pairings = pairing
+        #self.pairings.append(pairing)
+        # print(self.pairing)
+        # print(self.pairs)
+        print = type(pairing)
+        print('pairing')
+        print(self.pairings)
+        return
 
         # Fill in the rest
 
@@ -37,7 +43,8 @@ class MelonType(object):
 def make_melon_types():
     """Returns a list of current melon types."""
 
-    all_melon_types = []
+    all_melon_types = ['yw','cas','cren','musk']
+    
     #all_melon_types.append()
 
     # Fill in the rest
@@ -46,6 +53,7 @@ def make_melon_types():
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
+    
 
     # Fill in the rest
 
